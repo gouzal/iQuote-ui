eeb70e8bcf392bb8c2f003149999e0d6fd9f6afa
gouzal <larbi-inco@live.fr>
2020-07-26 20:59:23 +0100

    change API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://iquote-app.herokuapp.com/api'
+  apiUrl: 'https://iquote-api.herokuapp.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://iquote-app.herokuapp.com/api'
+  apiUrl: 'https://iquote-api.herokuapp.com/api'
 };
 
 /*
